2c4d5add16d0da8dc42e43d3fabf99d161ebaebc
Nikita Kudryakov <sir.nikitakudryakov@mail.ru>
2019-04-19 09:26:00 +0300

    Initial Commit

---
+++ GIF Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GIF Finder
+//
+//  Created by Никита Кудряков on 19/04/2019.
+//  Copyright © 2019 Никита Кудряков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
